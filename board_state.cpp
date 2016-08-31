6f5f984cff0a302a79403b8b905f192d0512aea7
bshankar <ebhavanishankar@gmail.com>
2016-08-31 16:48:30 +0530

    Use the implementation in http://citeseerx.ist.psu.edu/viewdoc/download?doi=10.1.1.51.7113&rep=rep1&type=pdf

--- board_state.cpp
+++ board_state.cpp
@@ -4,75 +4,35 @@
 using std::cout;
 using std::endl;
 
-board_state::board_state(int board_size) {
+board_state::board_state(row_t board_size) {
     if (board_size < MIN_BOARD || board_size > MAX_BOARD) {
         cout << "Valid board sizes are " << MIN_BOARD <<
              " to " << MAX_BOARD << endl;
         exit(1);
     }
     this->board_size = board_size;
-    init_tables();
+    FULL = (1 << board_size) - 1;
 }
 
-void board_state::init_tables() {
-    FULL = (~FULL) >> (MAX_BOARD - board_size);
-    for (auto col = 0; col < board_size; ++col) {
-        valid_cols[col] = FULL;
-        exclude[col] = FULL ^ (ONE << col);
-    }
-
-    for (auto dist = 0; dist < board_size - 1; ++dist)
-        for (auto col = 0; col < board_size; ++col) {
-            masks[MAX_BOARD * dist + col] = ONE << 2 * (dist + 1) |
-                                            ONE << (dist + 1) |
-                                            ONE;
-
-            // shift the masks properly
-            if (dist + 1 > col)
-                masks[MAX_BOARD * dist + col] >>= dist + 1 - col;
-            else if (dist + 1 < col)
-                masks[MAX_BOARD * dist + col] <<= col - dist - 1;
-
-            // take the complement
-            masks[MAX_BOARD * dist + col] = ~masks[MAX_BOARD * dist + col];
-            // remove overflows
-            masks[MAX_BOARD * dist + col] &= FULL;
-        }
-}
 
 void board_state::solve() {
-    search(0);
+    search(0, 0, 0);
     cout << "Board size " << board_size << ": "
          << solutions << " solutions found." << endl;
 }
 
-void board_state::search(int row) {
-    if (row == board_size) {
+void board_state::search(board_t left_diag, board_t col,
+                         board_t right_diag) {
+    if (col == FULL) {
         ++solutions;
         return;
     }
 
-    // critical loop
-    // Ideas to optimze this loop:
-    // 1. Search only for primitive solutions >= 2x
-    // 2. Use openmp for parallel search: jx
-    // 3. Denser encoding of constraints preferably with 64bit arithmetic >= 2x
-    // 4. Intelligently select a column for a row that has no violations
-    while (valid_cols[row]) {
-        int col = __builtin_ffsl(valid_cols[row]) - 1;
-        // exclude this column
-        valid_cols[row] &= exclude[col];
-        board_t tmp[board_size];
-        for (auto dist = 0; dist < board_size - row - 1; ++dist) {
-            // back up old values
-            tmp[row + dist + 1] = valid_cols[row + dist + 1];
-            // mask all conflicting columns
-            valid_cols[row + dist + 1] &= masks[MAX_BOARD * dist + col];
-        }
-
-        search(row + 1);
-        // restore backed up values
-        for (auto dist = 0; dist < board_size - row - 1; ++dist)
-            valid_cols[row + dist + 1] = tmp[row + dist + 1];
+    board_t empty_slots = ~(col | left_diag | right_diag);
+    while (empty_slots & FULL) {
+        board_t empty_slot = empty_slots & -empty_slots;
+        empty_slots -= empty_slot;
+        search((left_diag | empty_slot) >> 1, col | empty_slot,
+               (right_diag | empty_slot) << 1);
     }
 }
--- board_state.h
+++ board_state.h
@@ -6,25 +6,20 @@
 #define MIN_BOARD 4
 #define MAX_BOARD 64
 typedef uint64_t board_t;
+typedef uint16_t row_t;
 
 class board_state {
 public:
-    board_state(int board_size);
+    board_state(row_t board_size);
     void solve();
 private:
-    int board_size;
+    row_t board_size;
     board_t ONE  = 1,
-            FULL = 0; // correct its value later
-    board_t valid_cols[MAX_BOARD] = {};
-    // masks[distance][column]
-    board_t masks[(MAX_BOARD - 1)*MAX_BOARD] = {};
-    // exclude a column as it was already searched
-    board_t exclude[MAX_BOARD];
-    board_t solutions = 0;
+            FULL = 0, // correct its value later
+            solutions = 0;
 
-    // pre calculate and store all the constraints
-    void init_tables();
-    void search(int row);
+    void search(board_t left_diag,
+                board_t col, board_t right_diag);
 };
 
 #endif /* end of include guard: BOARD_STATE_H_MZEYWVGT */
